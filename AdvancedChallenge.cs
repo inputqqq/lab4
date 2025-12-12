b1406420086229b4ecfe565f0671e3c08a161156
inputqqq <dimabropro68@gmail.com>
2025-12-12 19:09:39 +0700

    Вызов конструктора базового класса,Перегрузка с вызовом base и без,множественное наследование

---
+++ AdvancedChallenge.cs
@@ -0,0 +1,37 @@
+﻿public interface IResettable
+{
+    void Reset();
+}
+
+
+public class AdvancedChallenge : Challenge, IResettable
+{
+    public int Difficulty { get; set; }
+
+
+    public AdvancedChallenge(string id, string title, string desc, int diff)
+    : base(id, title, desc) // ВЫЗОВ БАЗОВОГО КОНСТРУКТОРА
+    {
+        Difficulty = diff;
+    }
+
+
+    public override void ShowChallengeInfo()
+    {
+        base.ShowChallengeInfo(); // вызов базовой версии
+        Console.WriteLine($"Difficulty: {Difficulty}");
+    }
+
+
+    public override double GetProgress(string userId) // перегрузка без вызова base
+    {
+        return 80.0;
+    }
+
+
+    public void Reset()
+    {
+        Participants.Clear();
+        Console.WriteLine("Challenge Reset!");
+    }
+}
